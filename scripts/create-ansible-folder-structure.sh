701127edc66b6bd7a8bb3de151c4d8032b33b70a
Víctor Moreno Jiménez <victormoreno@correo.ugr.es>
2020-03-27 11:25:05 +0100

    Restructured project with new folder structure. From Ansible best practices

---
+++ scripts/create-ansible-folder-structure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Fill this variable with the roles you need
+
+roles=(install_image)
+
+
+# Everything will be under the ansible folder
+mkdir ../ansible && cd ../ansible
+
+# Inventory directory for staging and production
+mkdir -p inventories/production/host_vars
+mkdir -p inventories/production/group_vars
+mkdir -p inventories/staging/host_vars
+mkdir -p inventories/staging/group_vars
+
+# Inventory file for staging and production
+touch inventories/production/hosts
+touch inventories/staging/hosts
+
+# Optional directorys (Rarely used)
+mkdir {library,module_utils,filter_plugins}
+
+# Playbooks directory (Not in the official docs)
+mkdir playbooks
+
+# Roles directory, below Roles everything will be created with ansible-galaxy init
+mkdir roles && cd roles
+
+# Create roles 
+for i in ${roles[@]}; do
+  ansible-galaxy init $i
+done
+
---
+++ scripts/create-new-roles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Check if there is at least one argument
+if [ $# -eq 0 ]; then
+  echo "Usage: ./create-new-roles.sh role1 role2 role3..."
+fi
+
+# First go to the roles directory
+cd ansible/roles
+
+# Iterate arguments and create all roles
+for role in "$@"; do
+  ansible-galaxy init $role
+done
